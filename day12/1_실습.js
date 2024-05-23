a0bf8ed33a55fa5652d0ae6c6e56a7c26265dd60
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-23 11:26:52 +0900

    2교시 종료후 저장

--- day12/1_실습.js
+++ day12/1_실습.js
@@ -15,9 +15,21 @@
         (백엔드)
         1. 메모리 구성 
             아이디와 비밀번호를 저장하는 저장소 (C)
+            let idList = []
+            let pwList = []
         2. 기능/서비스 구성
             1. 회원가입 처리 == 함수
+                signup
+                [로직 순서도]
+                    1. 아이디/비밀번호 입력받는다.
+                    2. 입력받은 데이터를 각 배열에 저장한다.
+                    3. 안내 메시지
             2. 로그인 처리 == 함수
+                login
+                [로직 순서도]
+                    1. 아이디/비밀번호 입력받는다.
+                    2. 입력받은 데이터를 배열 내 존재하는지 비교 
+                    3. 안내 메시지
         3. 함수 구현 
 
         (프론트엔드)
@@ -32,3 +44,46 @@
         (백엔드/프론트엔드 연동)
         각 button에 JS 함수 연동 
 */
+
+// 1. 메모리 구성
+let idList = [];
+let pwList = [];
+
+// 2. 회원가입 함수 구성
+function signup() {
+    // 2-1
+    let signupId = document.querySelector('#signupId');
+    let signupPw = document.querySelector('#signupPw');
+    // 2-2
+    let id = signupId.value;
+    let pw = signupPw.value;
+    // 2-3 입력받은 데이터를 각 배열에 저장한다.
+    idList.push(id);
+    pwList.push(pw);
+    // 3-4 안내 메시지
+    alert('회원가입 성공');
+}
+
+// 2. 로그인 함수 구성
+function login() {
+    // 2-1
+    let loginId = document.querySelector('#loginId');
+    let loginPw = document.querySelector('#loginPw');
+    // 2-2
+    let id = loginId.value;
+    let pw = loginPw.value;
+    // 3-3 비교
+    // 3-1 아이디가 기존 배열 내 존재하는지 
+    let index = idList.indexOf(id);
+    if (index != -1) {
+        // 3-2 아이디가 존재하면 패스워드 검증
+        // 만약에 아이디가 위치한 인덱스의 패스워드도 위치한 인덱스의 패스워드가 입력받은 패스워드와 같으면 
+        if (pwList[index] == pw) {
+            alert('로그인 성공');
+        } else {
+            alert('패스워드가 다릅니다.');
+        }
+    } else {
+        alert('없는 아이디 입니다.');
+    }
+}
