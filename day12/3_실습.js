028f74bba447aac6187d6947214c82e981c0a118
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-23 16:09:58 +0900

    ttt

--- day12/3_실습.js
+++ day12/3_실습.js
@@ -1,1 +1,46 @@
-// sdaflsa;kdjf aa
+let todoList = [];
+let todoSwitch = [];
+
+function add() {
+    let todoInput = document.querySelector('#todoList');
+
+    let todo = todoInput.value;
+
+    todoList.push(todo);
+
+    alert('항목 저장 성공');
+
+    print();
+}
+
+
+function print() {
+    let html = ``;
+
+    for (let i = 0; i < todoList.length; i++) {
+        let currentTodo = todoList[i];
+
+        html += `
+            <tr>
+                <td>${currentTodo}</td>
+                <td onclick="update(${i})"><button>변경</button></td>
+                <td onclick="remove(${i})"><button>삭제</button></td>
+            </tr>
+        `;
+    }
+
+    tableBox.innerHTML = html;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
