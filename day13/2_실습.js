199a16d14919e9adb93f598dc676991296133f1d
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-24 17:32:32 +0900

    dddd

--- day13/2_실습.js
+++ day13/2_실습.js
@@ -12,11 +12,12 @@ function carIn(x) { //입차
     let car = document.querySelector('#inputCar').value; console.log('car' + car);
     let currentTime = new Date(); console.log('currentTime1' + currentTime);
     currentTime = (currentTime.getHours() * 60) + currentTime.getMinutes(); console.log('currentTime2' + currentTime);
+    console.log('carNum' + carNum);
     if (car == '') { message('차량 번호가 잘못되었습니다.'); }
-    else if (carNum.includes(car) == false && carNum[x] == '') {
+    else if (carNum.includes(car) == false && carNum[x] == undefined) {
         carNum[x] = car; isParked[x] = true; carTime[x] = currentTime; message('입차성공'); console.log('success');
     } else { message('입차불가, 차가 이미 존재합니다'); console.log('fail'); }
-    console.log(carNum);
+    console.log(carNum, isParked);
     menuPrint();
 }
 
@@ -26,7 +27,7 @@ function carOut(x) { //출차
     let outTime = (currentTime.getHours() * 60) + currentTime.getMinutes(); // 분으로 환산된 현재시간
     let cost = (carTime[x] - outTime) * 100;
     if (carNum.includes(car) == true) {
-        message(`출차 완료되었습니다. 요금은 ₩${cost.toLocaleString('ko-KR')}원입니다`);
+        message(`출차 완료되었습니다. 요금은 ₩${cost.toLocaleString('ko-KR')}원입니다`); carNum[x] = undefined;
     } else { message('차량번호가 잘못되었습니다'); }
     menuPrint();
 }
