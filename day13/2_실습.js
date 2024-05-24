6cfc5c9cc6beca65ebdf0d54f9068cbb38c3dbc4
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-24 16:48:29 +0900

    ㅇㄹㅇㄹㅇ

--- day13/2_실습.js
+++ day13/2_실습.js
@@ -1,1 +1,44 @@
-// 
+carTime = [, , , , , , , , , , , , , , , , , , ,]; //입차시간
+isParked = []; //입차여부
+for (i = 0; i < 20; i++) { //isParked = false X 20
+    isParked.push(false);
+}
+console.log(isParked);
+carNum = [, , , , , , , , , , , , , , , , , , ,]; //주차된 차 번호
+currentTime = ''; //현재시간
+
+
+function parkSlot(x) { //입차
+    let car = document.querySelector('#inputCar').value;
+    if (carNum.includes(car) == false) {
+        carNum[x] = car; isParked[x] = true; message('입차성공');
+    } else { message('입차불가, 차가 이미 존재합니다'); }
+    menuPrint();
+}
+
+function carOut(x) { //출차
+    let car = document.querySelector('#inputCar').value;
+    if (carNum.includes(car) == true) {
+
+    } else { message('차량번호가 잘못되었습니다'); }
+    menuPrint();
+}
+
+function menuPrint() { //주차칸 표시
+    let parked = '';
+    let unparked = '';
+    let html = '';
+    for (let i = 0; i < 20; i++) {
+        if (isParked == true) { //color1
+            html += `<div id=parkSlot${i} >${i + 1}</div>`;
+        } else { //color2
+            html += `<div id=parkSlot${i} >${i + 1}</div>`;
+        }
+    }
+
+    document.querySelector('#parkWrap').innerHTML = html;
+}
+
+function message() { // 메시지칸 표시
+
+}
