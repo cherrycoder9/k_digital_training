ba5b75dfe95f540d623edad01250e7e7a750be95
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-27 18:19:45 +0900

    asdf

--- day13/2_실습.js
+++ day13/2_실습.js
@@ -1,8 +1,3 @@
-carTime = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; //입차시간
-isParked = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; //입차여부
-//console.log(isParked);
-carNum = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']; //주차된 차 번호
-//console.log(carNum);
 car = [];
 for (i = 0; i < 20; i++) { // car 배열 20칸
     car.push({ carTime: 0, isParked: 0, carNum: '' });
@@ -15,7 +10,11 @@ function carIn(x) { //입차
     let c = document.querySelector('#inputCar').value;
     let currentTime = new Date();
     currentTime = (currentTime.getHours() * 60) + currentTime.getMinutes();
-    if (c == '') { message('차량 번호가 잘못되었습니다.'); } // 차량번호 빈칸 X
+    let isDuplicate = false;
+    for (let i = 0; i < car.length; i++) {
+        if (car[i].carNum == c) { isDuplicate == true }
+    }
+    if (c == '' || isDuplicate == true) { message('차량 번호가 잘못되었습니다.'); } // 차량번호 빈칸 X
     else if (car[x].isParked == 1) { //차량 이미 있는지 비교
         message('입차불가, 차가 이미 존재합니다'); //차량이 이미 있음
     } else { // 차량이 없고 차량번호가 빈칸이 아님
@@ -25,25 +24,25 @@ function carIn(x) { //입차
 }
 function carOut() { //출차
     let c = document.querySelector('#inputCar').value;
-    let x = -1;
+    let index = -1;
 
     for (let i = 0; i < 20; i++) {          // 입력한 차량의 위치 확인
         if (car[i].carNum == c) {
-            x = i;
+            index = i;
             break;
         }
     }
 
     let currentTime = new Date();
     let outTime = (currentTime.getHours() * 60) + currentTime.getMinutes(); // 분으로 환산된 현재시간
-    ////console.log('outtime=', outTime, 'currentTime=', currentTime, 'cartime=', carTime[x]);
+    console.log('outtime=', outTime, 'currentTime=', currentTime, 'cartime=', carTime[index]);
 
-    let cost = Number((outTime - car[x].carTime) * 100);
+    let cost = Number((outTime - car[index].carTime) * 100);
     ////console.log('cost1= ', cost);
     if (cost == 0) { cost += 100; }
     ////console.log('cost2 =', cost);
-    if (x != -1) {
-        message(`출차 완료되었습니다. 요금은 ₩${cost.toLocaleString('ko-KR')}원입니다`); car[x].carNum = ''; car[x].isParked = 0; car[x].carTime = 0;
+    if (index != -1) {
+        message(`출차 완료되었습니다. 요금은 ₩${cost.toLocaleString('ko-KR')}원입니다`); car[index].carNum = ''; car[index].isParked = 0; car[index].carTime = 0;
     } else { message('차량번호가 잘못되었습니다'); }
     menuPrint(); ////console.log("carout =", carNum, isParked, carTime);
 }
