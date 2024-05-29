2ac3b6e8539dcf8e16684958d2a191c8694674b7
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-29 10:58:34 +0900

    sdfsdf

--- day16/과제1.js
+++ day16/과제1.js
@@ -10,15 +10,17 @@ function 회원등록() {
             if (회원정보[i].아이디 == signupID) {
                 infoMsg = "이미 가입된 아이디입니다.";
                 document.querySelector('#infoMsg').innerHTML = infoMsg;
+                document.querySelector('#signupID').value = '';
+                document.querySelector('#signupPW').value = '';
                 return;
             }
         }
         회원정보.push({ 아이디: signupID, 비밀번호: signupPW });
         infoMsg = "가입되었습니다.";
         document.querySelector('#infoMsg').innerHTML = infoMsg;
+        document.querySelector('#signupID').value = '';
+        document.querySelector('#signupPW').value = '';
     }
-    document.querySelector('#signupID').value = '';
-    document.querySelector('#signupPW').value = '';
 }
 
 function 로그인() {
@@ -30,14 +32,20 @@ function 로그인() {
         if (loginID == 회원정보[i].아이디 && loginPW == 회원정보[i].패스워드) {
             infoMsg = '로그인 성공했습니다.';
             document.querySelector('#infoMsg').innerHTML = infoMsg;
+            document.querySelector('#loginID').value = '';
+            document.querySelector('#loginPW').value = '';
             return;
         } else if (loginID == 회원정보[i].아이디 && loginPW != 회원정보[i].패스워드) {
             infoMsg = '비밀번호가 다릅니다.';
             document.querySelector('#infoMsg').innerHTML = infoMsg;
+            document.querySelector('#loginID').value = '';
+            document.querySelector('#loginPW').value = '';
             return;
         } else {
             infoMsg = '없는 아이디입니다.';
             document.querySelector('#infoMsg').innerHTML = infoMsg;
+            document.querySelector('#loginID').value = '';
+            document.querySelector('#loginPW').value = '';
         }
     }
 }
