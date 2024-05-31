ab43c5eedcf7b163c115a88d5321d45aee40528a
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-31 17:12:10 +0900

    ㅁㄴㅇㄹsd

--- day18/2_과제.js
+++ day18/2_과제.js
@@ -1,5 +1,11 @@
 let 인사리스트 = [];
+let depList = [];
 let 인사번호 = 0;
+
+
+
+
+
 function 인사등록() {
     console.log('인사등록() 불러오기 OK');
     let 이름 = document.querySelector(`#hrName`).value;
@@ -32,4 +38,52 @@ function 인사출력() {
     console.log('인사리스트', 인사리스트);
 
     document.querySelector(`#hrList`).innerHTML = 인사리스트화면저장;
-} 
+}
+
+function 부서배정하기() {
+    console.log('부서배정하기() 불러오기 OK');
+    let 날짜 = document.querySelector(`#test1`).value;
+    console.log('날짜', 날짜);
+    let 직책 = document.querySelector(`#test2`).value;
+}
+
+// 부서정보 추가 함수
+function 부서등록() {
+    let depNum = depList.length != 0 ? depList.length : 0;
+    let depNameInput = document.querySelector('#depName').value;
+    let depPhoneInput = document.querySelector('#depPhone').value;
+    let depInform = { depNum: depNum, depName: depNameInput, depPhone: depPhoneInput };
+
+    depList.push(depInform);
+
+    alert('부서 등록 성공');
+
+    부서출력();
+}
+
+// 부서정보 출력 함수
+function 부서출력() {
+    let depListTable = document.querySelector('#depList');
+    let html = '';
+
+    for (let i = 0; i < depList.length; i++) {
+        html += `
+                <tr>
+                    <td>${depList[i].depNum}</td>
+                    <td>${depList[i].depName}</td>
+                    <td>${depList[i].depPhone}</td>
+                    <td><button>선택</button></td>
+                </tr>
+                `;
+    }
+
+    depListTable.innerHTML = html;
+    console.log('부서출력 성공');
+}
+
+
+function 배정부서출력() {
+
+}
+
+
