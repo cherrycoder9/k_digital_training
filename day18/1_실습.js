a31e8ebf4a61591932259f37c36281e9f70a267e
cherrycoder9 <cherrycoder9@gmail.com>
2024-05-31 13:32:52 +0900

    ㄴㅇㄹㄴㅇㄹ

--- day18/1_실습.js
+++ day18/1_실습.js
@@ -13,6 +13,11 @@ let 제품목록 = [
 
 // 1. 제품 등록 함수: 등록 버튼을 클릭했을때
 function 제품등록() {
+    // !-------- localStorage 호출 --------
+
+
+
+
     // 1. 입력받은 값을 가져온다.
     let 제품명 = document.querySelector(`#제품명`).value;
     let 가격 = document.querySelector(`#가격`).value;
@@ -27,6 +32,12 @@ function 제품등록() {
     // 4. 가공된 데이터를 저장한다
     제품목록.push(제품);
     alert(`제품등록 성공`);
+
+    // !-------- localStorage 저장 --------
+
+
+
+
     제품출력();
 }
 
@@ -34,6 +45,8 @@ function 제품등록() {
 // JS가 열렸을때, 제품 등록/수정/삭제 했을때 
 제품출력();
 function 제품출력() {
+    // !-------- localStorage 호출 --------
+
     console.log(`제품출력()`);
     // 1. 어디에
     let 제품출력구역 = document.querySelector(`#제품출력구역`);
@@ -62,6 +75,8 @@ let 주문목록 = [
 
 // 3. 주문: 주문 버튼을 클릭했을때 
 const 주문등록 = (제품번호) => {
+    // !-------- localStorage 호출 --------
+
     console.log(`주문등록()`, 제품번호);
     // 1.
 
@@ -78,11 +93,19 @@ const 주문등록 = (제품번호) => {
     // alert(`주문성공`);
     console.log(주문목록);
 
+    // !-------- localStorage 저장 --------
+
+
     // 5. 주문버튼을 누르면 최종적으로 추가된 주문현황을 갱신
     주문출력();
 };
+
+
 주문출력();
 function 주문출력() {
+
+    // !-------- localStorage 호출 --------
+
     // 1.
     let 주문출력구역 = document.querySelector(`#주문출력구역`);
     // 2. 
@@ -117,25 +140,70 @@ function 제품조회(제품번호) {
 // 5. 자동주문: 실행조건 - 자동주문을 클릭했을때 
 function 자동주문() {
     console.log(`자동주문()`);
-    // 주문할 제품번호를 난수 생성 (현재 제품목록에 존재하는 제품번호만)
-    // 제품번호 1 ~ 마지막 제품번호 
-    let 끝제품번호 = 제품목록[제품목록.length - 1].제품번호; // 2
 
-    // 난수 생성: Math.random() * 끝번호 + 시작번호
-    let 제품번호 = Math.floor(Math.random()) * 끝제품번호 + 1;
-    console.log(제품번호);
     // 특정 주기마다 함수를 실행해주는 라이브러리 함수
     // 첫번째 매개변수는 함수를 호출하는 곳이 아니라 함수를 정의하는곳임
-    setInterval(() => { 주문등록(1); }, 500);
-
+    setInterval(() => {
+        // 주문할 제품번호를 난수 생성 (현재 제품목록에 존재하는 제품번호만)
+        // 제품번호 1 ~ 마지막 제품번호 
+        let 끝제품번호 = 제품목록[제품목록.length - 1].제품번호; // 2
+        // 난수 생성: Math.random() * 끝번호 + 시작번호
+        let 제품번호 = parseInt((Math.random()) * 끝제품번호 + 1);
+        console.log(제품번호);
+        주문등록(제품번호);
+    }, 500);
 }
 
+// 6. localStorage(): JS 메모리가 아닌 브라우저(크롬) 프로그램 메모리 저장
+// .setItem(key, value)         .getItem(key)
+// 1. 제품등록, 2. 제품출력 3. 주문등록 4. 주문출력
+// setItem: 1(제품목록)            3(주문목록)
+// getItem: 1(제품목록) 2(제품목록) 3(주문목록) 4(주문목록)
+function 데이터최신화(처리번호) {
+    // ================== 제품목록 ==================
+    // 2. 호출 localStorage.getItem(key);
+    제품목록 = JSON.parse(localStorage.getItem("productList"));
+    if (제품목록 == null) {
+        제품목록 = [];
+    }
+    // 1. 저장 localStorage.setItem(key, value);
+    localStorage.setItem("productList", JSON.stringify(제품목록));
+
+    // ================== 주문목록 ==================
+    주문목록 = JSON.parse(localStorage.getItem("orderList"));
+    if (주문목록 == null) {
+        주문목록 = [];
+    }
+    localStorage.setItem("orderList", JSON.stringify(주문목록));
+
+
+    if (처리번호 == 1) {
+        // ================== 제품목록 ==================
+
+    } else if (처리번호 == 2) {
+
+    } else if (처리번호 == 3) {
+        // ================== 주문목록 ==================
+
+    } else if (처리번호 == 4) {
+
+    } else {
+
+    }
+
+
+
+    // 6-1 제품목록을 브라우저(프로그램)의 localStorage 저장
+    // 6-2 제품목록을 브라우저(프로그램)의 localStorage 호출  
+    // 6-3 주문목록 브라우저(프로그램)의 localStorage 저장
+    // 6-4 주문목록 브라우저(프로그램)의 localStorage 호출  
+}
 
 // 날짜/숫자 2자리 변환 함수
 function 자릿수변환(변환할값) {
     if (변환할값 < 10) {
         // 만약에 월/일/시/분 각각 매개변수가 10보다 작으면 앞에 '0' 연결하기
-        변환할값 += '0';
+        변환할값 = '0' + 변환할값;
     }
     // 반환
     console.log('변환할값', 변환할값);
